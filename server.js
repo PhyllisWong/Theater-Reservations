eed914ad6c4a7667eaaace7c19ee51b12dcc3f7c
Phyllis Wong <wong.phyllism@gmail.com>
2018-09-13 15:12:08 -0700

    Add mongodb connection without mongoose

--- server.js
+++ server.js
@@ -2,33 +2,38 @@
 const express = require('express');
 const app = express();
 const assert = require('assert');
-const mongoDB = require('mongodb');
-
-
-// Mongoose Connection
-/* Mongo Database Connection */
 const MongoClient = require('mongodb').MongoClient;
 
 // Connection URL
 const mongoUri = process.env.MONGODB_URI || "mongodb://localhost:27017/theater-reservation";
 
 // Database Name
-const dbName = 'theaters';
+const dbName = 'theater-reservations';
 
-// Use connect method to connect to the server
-MongoClient.connect(mongoUri, (err, client) => {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
+// Initialize db
+let db;
 
-  const db = client.db(dbName);
-
-  client.close();
-});
 
 // ******  ROUTES  ****** //
 // Example stub:
 app.get('/theater/name-of-route', function(req, res) {
   res.json({'stub': `[${req.originalUrl}] Endpoint works! Replace me in Part 2.`});
 });
+
+
+
+/* Mongo Database Connection */
+// Use connect method to connect to the server
+MongoClient.connect(mongoUri, (err, client) => {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  db = client.db(dbName);
+  db.collection('theaters');
+  db.collection('sessions');
+
+  client.close();
+});
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
